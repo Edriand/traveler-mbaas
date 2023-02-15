af1456453553bce822e6aca5ff7ea9b11996575c
Edriand <adriandato2000@gmail.com>
2023-02-15 19:57:51 +0100

    Mltiple tables stack added

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { TablesStack } from '../lib/dynamodb-stack';
+import { Table } from '../interfaces/table.interface';
 
 const app = new cdk.App();
 
-new TablesStack(app, 'TablesStack', {});
+const tables: Table[] = [
+    {name: 'person', pk: 'id', sk: 'city'},
+    {name: 'coords', pk: 'id', sk: 'coords'}
+];
+
+new TablesStack(app, 'TablesStack', tables, {});
--- lib/dynamodb-stack.ts
+++ lib/dynamodb-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps, aws_dynamodb } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Table } from '../interfaces/table.interface';
 
 export class TablesStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, tables: Table[], props?: StackProps) {
     super(scope, id, props);
 
-    const table_person = new aws_dynamodb.Table(this, 'Table', {
-      tableName: `${this.node.tryGetContext('ID')}-${this.node.tryGetContext('ENV')}-table-person`,
-      partitionKey: { name: 'id', type: aws_dynamodb.AttributeType.STRING },
-      sortKey: { name: 'city', type: aws_dynamodb.AttributeType.STRING },
-      billingMode: aws_dynamodb.BillingMode.PAY_PER_REQUEST,
-      tableClass: aws_dynamodb.TableClass.STANDARD,
+    tables.map(t => {
+      new aws_dynamodb.Table(this, t.name, {
+        tableName: `${this.node.tryGetContext('ID')}-${this.node.tryGetContext('ENV')}-table-${t.name}`,
+        partitionKey: { name: t.pk, type: aws_dynamodb.AttributeType.STRING },
+        sortKey: { name: t.sk, type: aws_dynamodb.AttributeType.STRING },
+        billingMode: aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+        tableClass: aws_dynamodb.TableClass.STANDARD,
+      });
     });
   }
 }
